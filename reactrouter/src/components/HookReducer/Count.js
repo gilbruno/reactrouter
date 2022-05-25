3a651e73645bf2dddbd7cf3fc4f55b094e00bc7f
Gilles Bruno <gilles_bruno@gmx.fr>
2022-05-25 14:19:57 +0200

    Simple Example of useReducer hooks

--- reactrouter/src/components/HookReducer/Count.js
+++ reactrouter/src/components/HookReducer/Count.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+
+const initialState = 0
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;    
+    case 'reset':
+        return initialState;      
+    default : 
+      return state;    
+  }
+
+}
 
 function Count() {
+
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="container">Count</div>
+    <div className="container">
+      <h1>
+      {count}
+      </h1>
+      
+      <button className='btn btn-success m-3' onClick={() => dispatch('increment')}>+</button>
+      <button className='btn btn-danger m-3' onClick={() => dispatch('decrement')}>-</button>
+      <button className='btn btn-primary m-3' onClick={() => dispatch('reset')}>Reset</button>
+      
+    </div>  
+      
   )
 }
 
